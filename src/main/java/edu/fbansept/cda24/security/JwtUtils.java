9079bebf27e3469d3e160d8c5f913a3eca10e2d8
fbansept <bansept.franck@gmail.com>
2024-04-22 13:53:30 +0200

    add role in jwt

--- src/main/java/edu/fbansept/cda24/security/JwtUtils.java
+++ src/main/java/edu/fbansept/cda24/security/JwtUtils.java
@@ -2,17 +2,23 @@ package edu.fbansept.cda24.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class JwtUtils {
 
     public String generateToken(UserDetails userDetails) {
 
+        SimpleGrantedAuthority role = (SimpleGrantedAuthority)userDetails.getAuthorities().toArray()[0];
+
         return Jwts.builder()
                 .signWith(SignatureAlgorithm.HS256, "azerty")
                 .setSubject(userDetails.getUsername())
+                .addClaims(Map.of("admin", role.getAuthority().equals("ROLE_ADMIN") ? 1 : 0))
                 .compact();
     }
 
